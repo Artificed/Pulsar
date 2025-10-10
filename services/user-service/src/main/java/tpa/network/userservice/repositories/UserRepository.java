55936d8bb23c7615e38fab4f7f1fa6013f8cea89
Artificed <paultsai.id@gmail.com>
2025-10-10 12:41:23 +0700

    revert: removed boilerplate user crud

--- services/user-service/src/main/java/tpa/network/userservice/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package tpa.network.userservice.repositories;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-import tpa.network.userservice.models.User;
-
-@Repository
-public interface UserRepository extends MongoRepository<User, String> {
-}
