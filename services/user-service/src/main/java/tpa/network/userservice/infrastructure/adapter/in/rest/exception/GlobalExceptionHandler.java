5128fab6bda185dc41728298f843cfd9c04170e3
Artificed <paultsai.id@gmail.com>
2025-10-13 15:01:30 +0700

    feat: better error response

--- services/user-service/src/main/java/tpa/network/userservice/infrastructure/adapter/in/rest/exception/GlobalExceptionHandler.java
+++ services/user-service/src/main/java/tpa/network/userservice/infrastructure/adapter/in/rest/exception/GlobalExceptionHandler.java
@@ -2,35 +2,40 @@ package tpa.network.userservice.infrastructure.adapter.in.rest.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import tpa.network.userservice.domain.exception.*;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(UserNotFoundException.class)
-    public ResponseEntity<String> handleUserNotFound(UserNotFoundException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    public ResponseEntity<ErrorResponse> handleUserNotFound(UserNotFoundException ex) {
+        var response = new ErrorResponse(HttpStatus.NOT_FOUND.value(), ex.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(EmailTakenException.class)
-    public ResponseEntity<String> handleEmailTaken(EmailTakenException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.CONFLICT);
+    public ResponseEntity<ErrorResponse> handleEmailTaken(EmailTakenException ex) {
+        var response = new ErrorResponse(HttpStatus.CONFLICT.value(), ex.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.CONFLICT);
     }
 
     @ExceptionHandler(InvalidUsernameFormatException.class)
-    public ResponseEntity<String> handleInvalidUsernameFormat(InvalidUsernameFormatException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<ErrorResponse> handleInvalidUsernameFormat(InvalidUsernameFormatException ex) {
+        var response = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), ex.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(InvalidEmailFormatException.class)
-    public ResponseEntity<String> handleInvalidEmailFormat(InvalidEmailFormatException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<ErrorResponse> handleInvalidEmailFormat(InvalidEmailFormatException ex) {
+        var response = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), ex.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(InvalidPasswordFormatException.class)
-    public ResponseEntity<String> handleInvalidPasswordFormat(InvalidPasswordFormatException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<ErrorResponse> handleInvalidPasswordFormat(InvalidPasswordFormatException ex) {
+        var response = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), ex.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 }
