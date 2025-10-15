82d3f32e046a151e1417793b628af637752c0204
Artificed <paultsai.id@gmail.com>
2025-10-15 12:54:10 +0700

    chore: removed test routes for user and event service

--- services/event-service/src/main/java/tpa/network/eventservice/EventServiceApplication.java
+++ services/event-service/src/main/java/tpa/network/eventservice/EventServiceApplication.java
@@ -2,19 +2,11 @@ package tpa.network.eventservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class EventServiceApplication {
 
-    @RequestMapping("/")
-    public String index() {
-        return "Test";
-    }
-
-    public static void main(String[] args) {
+   public static void main(String[] args) {
         SpringApplication.run(EventServiceApplication.class, args);
     }
 
