16d16eb5f512852d616a1e660344d07d0259689b
Artificed <paultsai.id@gmail.com>
2025-10-15 12:42:54 +0700

    feat: defined event query and command controllers

---
+++ services/event-service/src/main/java/tpa/network/eventservice/infrastructure/adapter/in/rest/query/EventQueryController.java
@@ -0,0 +1,63 @@
+package tpa.network.eventservice.infrastructure.adapter.in.rest.query;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import tpa.network.eventservice.domain.port.in.query.GetAllEventsQuery;
+import tpa.network.eventservice.domain.port.in.query.GetEventByIdQuery;
+import tpa.network.eventservice.infrastructure.adapter.in.rest.query.dto.EventResponse;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/events")
+@RequiredArgsConstructor
+public class EventQueryController {
+    private final GetAllEventsQuery getAllEventsQuery;
+    private final GetEventByIdQuery getEventByIdQuery;
+
+    @GetMapping
+    public ResponseEntity<List<EventResponse>> getAllEvents() {
+        var events = getAllEventsQuery.execute();
+        
+        var response = events.stream()
+                .map(event -> new EventResponse(
+                        event.getId(),
+                        event.getTitle(),
+                        event.getDescription(),
+                        event.getImageUrl(),
+                        event.getDatetime(),
+                        event.getDurationMinutes(),
+                        event.getLocation(),
+                        event.getPrice(),
+                        event.getCapacity(),
+                        event.getSeatsAvailable()
+                ))
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<EventResponse> getEventById(@PathVariable String id) {
+        var event = getEventByIdQuery.execute(
+                new GetEventByIdQuery.GetEventByIdRequest(id)
+        );
+
+        var response = new EventResponse(
+                event.getId(),
+                event.getTitle(),
+                event.getDescription(),
+                event.getImageUrl(),
+                event.getDatetime(),
+                event.getDurationMinutes(),
+                event.getLocation(),
+                event.getPrice(),
+                event.getCapacity(),
+                event.getSeatsAvailable()
+        );
+
+        return ResponseEntity.ok(response);
+    }
+}
