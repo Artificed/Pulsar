47ee5e1e12241c850d3d63388fe1acc8b47c1a4e
Artificed <paultsai.id@gmail.com>
2025-11-01 14:11:49 +0700

    feat: defined domain ports and exceptions

---
+++ services/booking-service/src/main/java/tpa/network/bookingservice/domain/port/out/command/BookingCommandRepositoryPort.java
@@ -0,0 +1,10 @@
+package tpa.network.bookingservice.domain.port.out.command;
+
+import tpa.network.bookingservice.domain.model.booking.Booking;
+import tpa.network.bookingservice.domain.model.shared.Id;
+
+public interface BookingCommandRepositoryPort {
+    Booking insert(Booking booking);
+    Booking update(Booking booking);
+    Id deleteById(String id);
+}
