d508767a534a8c22e8dd0dc0859bfd50643385ee
Artificed <paultsai.id@gmail.com>
2025-11-06 10:18:51 +0700

    fix: completed global grpc exception handler

--- backend/src/main/java/tpa/network/backend/infrastructure/adapter/in/grpc/interceptor/GlobalGrpcExceptionHandler.java
+++ backend/src/main/java/tpa/network/backend/infrastructure/adapter/in/grpc/interceptor/GlobalGrpcExceptionHandler.java
@@ -3,6 +3,7 @@ package tpa.network.backend.infrastructure.adapter.in.grpc.interceptor;
 import io.grpc.Status;
 import net.devh.boot.grpc.server.advice.GrpcAdvice;
 import net.devh.boot.grpc.server.advice.GrpcExceptionHandler;
+import tpa.network.backend.domain.exception.*;
 import tpa.network.backend.domain.exception.BookingNotFoundException;
 import tpa.network.backend.domain.exception.InvalidIdFormatException;
 import tpa.network.backend.domain.exception.InvalidQuantityException;
@@ -15,10 +16,31 @@ public class GlobalGrpcExceptionHandler {
         return Status.NOT_FOUND.withDescription(e.getMessage()).withCause(e);
     }
 
+    @GrpcExceptionHandler(UserNotFoundException.class)
+    public Status handleUserNotFoundException(UserNotFoundException e) {
+        return Status.NOT_FOUND.withDescription(e.getMessage()).withCause(e);
+    }
+
+    @GrpcExceptionHandler(EventNotFoundException.class)
+    public Status handleEventNotFoundException(EventNotFoundException e) {
+        return Status.NOT_FOUND.withDescription(e.getMessage()).withCause(e);
+    }
+
+    @GrpcExceptionHandler(EmailTakenException.class)
+    public Status handleEmailTakenException(EmailTakenException e) {
+        return Status.ALREADY_EXISTS.withDescription("Email is already taken").withCause(e);
+    }
+
     @GrpcExceptionHandler({
             InvalidIdFormatException.class,
-            InvalidIdFormatException.class,
-            InvalidQuantityException.class
+            InvalidQuantityException.class,
+            InvalidPasswordFormatException.class,
+            InvalidEmailFormatException.class,
+            InvalidUsernameFormatException.class,
+            InvalidCapacityException.class,
+            InvalidPriceException.class,
+            InvalidTitleException.class,
+            InvalidDateTimeException.class
     })
     public Status handleValidationExceptions(RuntimeException e) {
         return Status.INVALID_ARGUMENT.withDescription(e.getMessage()).withCause(e);
