b8282b30de016fd2ac272f70948e19dda1db97aa
Artificed <paultsai.id@gmail.com>
2025-11-10 14:23:27 +0700

    refactor: better file structure for all pages

---
+++ frontend/src/app/(main)/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <html>
+      <body>
+        <div>
+          <h1>Welcome to the Home Page</h1>
+        </div>
+      </body>
+    </html>
+  );
+}
