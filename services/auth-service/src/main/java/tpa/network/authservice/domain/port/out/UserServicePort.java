d3966c48ce4bc8ca77a07eb1feab648b209b170d
Artificed <paultsai.id@gmail.com>
2025-11-14 15:02:35 +0700

    feat: register use case for auth service

--- services/auth-service/src/main/java/tpa/network/authservice/domain/port/out/UserServicePort.java
+++ services/auth-service/src/main/java/tpa/network/authservice/domain/port/out/UserServicePort.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 public interface UserServicePort {
     Optional<UserDto> getUserByEmail(String email);
     Optional<UserDto> getUserById(String userId);
+    String createUser(String username, String email, String passwordHash);
     
     record UserDto(String id, String username, String email, String passwordHash) {}
 }
